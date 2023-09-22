d2f95da0e8ce351b642a09b14ece3b7ee0968bf8
Nemanja <nemanja.vasic@productdock.com>
2023-09-22 14:37:01 +0200

    Add nearest neighbors sketch

--- main.js
+++ main.js
@@ -1,45 +1,79 @@
 import p5 from "p5";
 
+const redPoint = { x: 200, y: 200 };
 let points = [];
-let dis;
+let neighbors = [];
+let button;
 
-const euclideanDistanceBetweenTwoPoints = new p5((p) => {
-  function euclideanDistance(pointOne, pointTwo) {
-    let diffX = pointTwo.x - pointOne.x;
-    let diffY = pointTwo.y - pointOne.y;
+const nearestNeighbors = new p5((p) => {
+  function euclideanDistance(otherPoint) {
+    let diffX = redPoint.x - otherPoint?.x;
+    let diffY = redPoint.y - otherPoint?.y;
+
     let sum = diffX * diffX + diffY * diffY;
+    return p.sqrt(sum);
+  }
 
-    dis = p.sqrt(sum);
-    return dis;
+  function findNearestNeighbors() {
+    if (points.length >= 5) {
+      for (let i = 0; i < points.length; i++) {
+        let dis = euclideanDistance(points[i]);
+        neighbors.push({ x: points[i].x, y: points[i].y, distance: dis });
+      }
+
+      console.log(
+        "neighbors sorted",
+        neighbors.map((n) => n.distance).sort((a, b) => a - b),
+      );
+      return neighbors.sort((a, b) => a.distance - b.distance);
+    }
+
+    return [];
+  }
+
+  function paintNearestNeighbors() {
+    const nearest = findNearestNeighbors().slice(0, 5);
+
+    // Change the color of the nearest points to blue
+    for (const neighbor of nearest) {
+      const index = points.findIndex(
+        (point) => point.x === neighbor.x && point.y === neighbor.y,
+      );
+      if (index !== -1) {
+        points[index].color = "blue"; // Add color property to points
+      }
+    }
   }
 
   p.setup = () => {
-    p.createCanvas(400, 400).parent("one");
+    p.createCanvas(400, 400).parent("two");
+
+    button = p.createButton("Calculate five nearest neighbors of RED point");
+    button.mousePressed(paintNearestNeighbors);
   };
 
   p.draw = () => {
     p.background(220);
-    p.text("Click on the canvas to create two points", p.width / 6, 10);
+    p.fill("black");
+    p.text(
+      "Click on the canvas to create five or more points around RED point",
+      10,
+      10,
+    );
+    p.fill("red");
+    p.ellipse(redPoint.x, redPoint.y, 30, 30);
 
     if (points.length > 0) {
       for (var i = 0; i < points.length; i++) {
+        p.fill(points[i].color || "white");
         p.ellipse(points[i].x, points[i].y, 10, 10);
       }
-    }
-
-    if (points.length >= 2) {
-      dis = euclideanDistance(
-        { x: points[0].x, y: points[0].y },
-        { x: points[1].x, y: points[1].y },
-      );
-      p.text(`Euclidean Distance between points: ${dis}`, p.width / 10, 30);
     }
   };
 
   p.mousePressed = () => {
-    if (points.length >= 2) {
-      points = [];
+    if (p.mouseX <= 400 && p.mouseY <= 400) {
+      points.push({ x: p.mouseX, y: p.mouseY });
     }
-    points.push({ x: p.mouseX, y: p.mouseY });
   };
 });
