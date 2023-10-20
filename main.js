9830f02479eac7ab51f953b45fbab998144e4ec3
Nemanja <nemanja.vasic@productdock.com>
2023-10-20 14:46:17 +0200

    Add second degree polynomial regression

--- main.js
+++ main.js
@@ -68,7 +68,7 @@ const nearestNeighbors = new p5((p) => {
     canvas.elt.className = "canvas";
 
     let buttonsWrapper = p.createDiv();
-    calculateButton = p.createButton("Nearest neighbors of orange point");
+    calculateButton = p.createButton("Nearest neighbors");
     resetButton = p.createButton("Reset");
 
     buttonsWrapper.elt.appendChild(calculateButton.elt);
@@ -229,5 +229,116 @@ const gradientDescentCanvas = new p5((p) => {
     y2 = p.map(y2, 0, 1, p.height, 0);
 
     p.line(x1, y1, x2, y2);
+  }
+});
+
+// Polynomial Regression
+
+const gPointsPR = [];
+const learningRatePR = 0.8;
+let m3 = 0;
+let m1 = 0;
+let m2 = 0;
+let curveVertices = [];
+
+function gradientDescentPolynomialRegression(m1, m2, m3) {
+  let pointsLength = gPointsPR.length;
+  let partialDerivativeOfM1 = 0;
+  let partialDerivativeOfM2 = 0;
+  let partialDerivativeOfM3 = 0;
+
+  for (let i = 0; i < pointsLength; i++) {
+    let x = gPointsPR[i].x;
+    let y = gPointsPR[i].y;
+
+    const predictedY = m1 * x * x + m2 * x + m3;
+    const error = predictedY - y;
+
+    partialDerivativeOfM1 += (1 / pointsLength) * error * x * x;
+    partialDerivativeOfM2 += (1 / pointsLength) * error * x;
+    partialDerivativeOfM3 += (1 / pointsLength) * error;
+  }
+
+  m1 -= learningRatePR * partialDerivativeOfM1;
+  m2 -= learningRatePR * partialDerivativeOfM2;
+  m3 -= learningRatePR * partialDerivativeOfM3;
+
+  return { m1, m2, m3 };
+}
+
+const polynomialRegressionCanvas = new p5((p) => {
+  function isInsideCanvas() {
+    return (
+      p.mouseX >= 0 &&
+      p.mouseX <= p.width &&
+      p.mouseY >= 0 &&
+      p.mouseY <= p.height
+    );
+  }
+
+  p.setup = () => {
+    let canvas = p.createCanvas(400, 400).parent("polynomialRegression");
+    canvas.elt.className = "canvas";
+  };
+
+  p.draw = () => {
+    p.background(220);
+
+    drawDots(p);
+
+    if (gPointsPR.length > 1) {
+      curveVertices = [];
+      for (let i = 0; i < gPointsPR.length; i++) {
+        const gd = gradientDescentPolynomialRegression(m1, m2, m3);
+        m1 = gd.m1;
+        m2 = gd.m2;
+        m3 = gd.m3;
+      }
+
+      generateQuadraticCurveVertices();
+      drawQuadraticCurve(p);
+    }
+  };
+
+  p.mousePressed = () => {
+    if (isInsideCanvas()) {
+      let x = p.map(p.mouseX, 0, p.width, 0, 1);
+      let y = p.map(p.mouseY, 0, p.height, 1, 0);
+
+      gPointsPR.push(p.createVector(x, y));
+    }
+  };
+
+  function drawDots(p) {
+    if (gPointsPR.length > 0) {
+      for (var i = 0; i < gPointsPR.length; i++) {
+        p.fill(255);
+        p.stroke(0);
+        let xPixel = p.map(gPointsPR[i].x, 0, 1, 0, p.width);
+        let yPixel = p.map(gPointsPR[i].y, 0, 1, p.height, 0);
+
+        p.ellipse(xPixel, yPixel, 10, 10);
+      }
+    }
+  }
+
+  function generateQuadraticCurveVertices() {
+    for (let x = 0; x < p.width; x++) {
+      let xScaled = p.map(x, 0, p.width, 0, 1);
+      let y = m1 * xScaled * xScaled + m2 * xScaled + m3;
+      let yPixel = p.map(y, 0, 1, p.height, 0);
+
+      curveVertices.push(p.createVector(x, yPixel));
+    }
+  }
+
+  function drawQuadraticCurve(p) {
+    p.beginShape();
+    p.noFill();
+    p.stroke(230, 126, 34);
+    for (let vertex of curveVertices) {
+      p.vertex(vertex.x, vertex.y);
+    }
+    p.endShape();
   }
 });
